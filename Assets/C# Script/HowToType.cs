a19b3bc2f4cc5f559d71f10ffe4d87c2bbe20a4b
FFooCuuSS <106500256+FFooCuuSS@users.noreply.github.com>
2025-04-17 23:21:28 +0900

    기초씬들, 폴더 정리
    제곧내

---
+++ Assets/C# Script/HowToType.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToType : MonoBehaviour
+{
+    /*
+        어디까지나 권고사항으므로 평소에 익숙한 방식이 있으시다면 하셔도 좋습니다
+     
+        변수명: howToType
+        함수명: HowToType
+        중괄호: (howToType)
+                {
+                    ;
+                }
+    */
+}
