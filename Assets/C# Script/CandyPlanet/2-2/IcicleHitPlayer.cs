8e02a090a51a958f13d27818e471546996d6f700
eju1121 <shdnr0506@naver.com>
2025-08-30 15:11:35 +0900

    2-1, 2-2, 2-10 minigame
    2-1, 2-2 minigame feedback
    2-10

--- Assets/C# Script/CandyPlanet/2-2/IcicleHitPlayer.cs
+++ Assets/C# Script/CandyPlanet/2-2/IcicleHitPlayer.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class IcicleHitPlayer : MonoBehaviour
 {
+    private MiniGame2_2 minigame_2_2;
+    public GameObject stage_2_2;
+
+    private void Start()
+    {
+        minigame_2_2 = stage_2_2.GetComponent<MiniGame2_2>();
+    }
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Icicle")
         {
-            Debug.Log("게임오버");
-        }
-        else if (coll.collider.tag == "Floor")
-        {
-            Debug.Log("바닥에 닿음");
-            Destroy(gameObject, 0.2f);
-
+            minigame_2_2.Failure();
         }
     }
 
---
+++ Assets/C# Script/CandyPlanet/2-2/MiniGame2_2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame2_2 : MiniGameBase
+{
+    protected override float TimerDuration => 5f;
+    protected override string MinigameExplain => "피해라!";
+
+    public override void StartGame()
+    {
+
+
+        // 추가 초기화
+        // 예: instructionText.text = MinigameExplain;
+    }
+
+    public void Succeed()
+    {
+        Success();
+    }
+    public void Failure()
+    {
+        Fail();
+    }
+}
