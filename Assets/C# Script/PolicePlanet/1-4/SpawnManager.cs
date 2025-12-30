860a6371b47caf5bcaa25956364fa86648ae68bb
이현기 <100886620+leehyunkee@users.noreply.github.com>
2025-12-30 19:39:30 +0900

    1230re

---
+++ Assets/C# Script/PolicePlanet/1-4/SpawnManager.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<GameObject> hatPrefabs;
+    public List<GameObject> mustachePrefabs;
+    public List<GameObject> glassesPrefabs;
+    public List<GameObject> montagePrefabs;
+
+    public Transform hatSpawnPoint;
+    public Transform mustacheSpawnPoint;
+    public Transform glassesSpawnPoint;
+    public Transform montageSpawnPoint;
+
+    public Minigame_1_4 minigame;
+
+    void Start()
+    {
+        SpawnRandom(hatPrefabs, hatSpawnPoint);
+        SpawnRandom(mustachePrefabs, mustacheSpawnPoint);
+        SpawnRandom(glassesPrefabs, glassesSpawnPoint);
+        SpawnRandom(montagePrefabs, montageSpawnPoint);
+    }
+
+    void SpawnRandom(List<GameObject> prefabList, Transform spawnPoint)
+    {
+        if (prefabList.Count == 0 || spawnPoint == null) return;
+
+        GameObject selected = prefabList[Random.Range(0, prefabList.Count)];
+        GameObject accObj = Instantiate(selected, spawnPoint);
+
+        Accessory acc = accObj.GetComponent<Accessory>();
+        if (acc != null)
+        {
+            //minigame.RegisterAccessory(acc);
+        }
+    }
+}
