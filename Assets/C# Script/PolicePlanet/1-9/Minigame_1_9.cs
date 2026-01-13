5e49f5c4473e6500e59c0fede58266b7d77971f3
hjexng <shdnr0506@naver.com>
2026-01-13 16:00:46 +0900

    1-9
    Edit completed

--- Assets/C# Script/PolicePlanet/1-9/Minigame_1_9.cs
+++ Assets/C# Script/PolicePlanet/1-9/Minigame_1_9.cs
@@ -5,16 +5,35 @@ using DG.Tweening;
 
 public class Minigame_1_9 : MiniGameBase
 {
+    [Header("Visual")]
+    [SerializeField] private Rope rope;
+
+    [Header("배경 깜빡임")]
+    [SerializeField] private GameObject brightBackground;
+
+    [SerializeField] private HandleMover_1_9 handleMover;
+
+    private bool canInput = false;
     private bool hasMissed = false;
 
     protected override float TimerDuration => 15f;
     protected override string MinigameExplain => "가동시켜라!";
 
-    private int touchCount = 0;
-    private const int targetTouchCount = 3;
+    private Tween blinkTween;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            handleMover.PlayStretch();
+        }
+    }
 
     public override void StartGame()
     {
+        canInput = false;
+        StopBlink();
+
         hasMissed = false;
     }
 
@@ -27,7 +46,6 @@ public class Minigame_1_9 : MiniGameBase
         Fail();
     }
 
-
     public override void OnRhythmEvent(string action)
     {
         Debug.Log($"{gameObject.name} 리듬메세지: {action}");
@@ -35,46 +53,58 @@ public class Minigame_1_9 : MiniGameBase
         // 이건 나중에 개별 미니게임에서 override하는 형태로
         switch (action)
         {
-            case "Tap":
-                Debug.Log("Tap");
-                //ShowTapPrompt();
+            case "Show":
+                PlayBlinkOnce();
                 break;
 
-            case "Hold":
-                Debug.Log("Hold");
-                //ShowHoldPrompt();
-                break;
-
-            case "Swipe":
-                Debug.Log("Swipe");
-                //ShowSwipePrompt();
+            case "Input":
+                canInput = true;
                 break;
         }
     }
 
+    public void OnScreenTouch()
+    {
+        if (!canInput) return;
+
+        canInput = false;
+
+        handleMover.PlayStretch();
+
+        OnPlayerInput();
+    }
+
+    // 판정 처리
     public override void OnJudgement(JudgementResult judgement)
     {
-        if (hasMissed) return;
+        StopBlink();
 
-        if (judgement == JudgementResult.Perfect ||
-            judgement == JudgementResult.Good)
-        {
-            Debug.Log("리듬 성공 → 미니게임 성공");
+        if (judgement == JudgementResult.Perfect || judgement == JudgementResult.Good)
             Success();
-        }
         else
-        {
-            //OnMiss();
-        }
+            Fail();
     }
 
-    //public void OnMiss()
-    //{
-    //    if (hasMissed) return;
+    private void PlayBlinkOnce()
+    {
+        if (brightBackground == null) return;
 
-    //    hasMissed = true;
-    //    Debug.Log("미스 발생 -> 실패");
+        // 각 Show마다 독립적인 Sequence 생성
+        Sequence blinkSequence = DOTween.Sequence();
+        blinkSequence.AppendCallback(() => brightBackground.SetActive(true));
+        blinkSequence.AppendInterval(0.5f); // 켜진 시간
+        blinkSequence.AppendCallback(() => brightBackground.SetActive(false));
 
-    //    Fail();
-    //}
+        // 자동 제거
+        blinkSequence.SetAutoKill(true);
+    }
+
+
+    private void StopBlink()
+    {
+        blinkTween?.Kill();
+
+        if (brightBackground != null)
+            brightBackground.SetActive(false);
+    }
 }
