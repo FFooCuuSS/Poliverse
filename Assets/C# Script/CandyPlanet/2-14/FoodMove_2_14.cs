0b4e20523d63ad4e27552eb854123bf05713f611
hjexng <ok685266@naver.com>
2026-07-26 22:29:08 +0900

    2-14

--- Assets/C# Script/CandyPlanet/2-14/FoodMove_2_14.cs
+++ Assets/C# Script/CandyPlanet/2-14/FoodMove_2_14.cs
@@ -1,38 +1,25 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodMove_2_14 : MonoBehaviour
 {
-    private Vector3 startPos;
-    private Vector3 targetPos = Vector3.zero;  // (0,0,0) 고정
-    private float moveDuration;
-    private float timer;
-    private bool stopped = false;
+    Tween moveTween;
 
-    public void Init(float duration)
+    public void Init(Transform player, float shieldRadius, float moveTime)
     {
-        startPos = transform.position;
-        moveDuration = duration;
-        timer = 0f;
-        stopped = false;
-    }
+        Vector3 dir = (player.position - transform.position).normalized;
 
-    void Update()
-    {
-        if (stopped) return;
+        // 방패 위치까지 이동
+        Vector3 targetPos = player.position - dir * shieldRadius;
 
-        timer += Time.deltaTime;
-        float t = timer / moveDuration;
-        transform.position = Vector3.Lerp(startPos, targetPos, t);
-
-        // 목표 도착하면 멈추기
-        if (t >= 1f)
-            stopped = true;
+        moveTween = transform.DOMove(targetPos, moveTime)
+                             .SetEase(Ease.Linear);
     }
 
     public void StopMovement()
     {
-        stopped = true;
+        moveTween?.Kill();
     }
 }
--- Assets/C# Script/CandyPlanet/2-14/FoodSpawn_2_14.cs
+++ Assets/C# Script/CandyPlanet/2-14/FoodSpawn_2_14.cs
@@ -1,36 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class FoodSpawn_2_14 : MonoBehaviour
 {
     public GameObject foodPrefab;
-    public int foodCount = 5;
-
     public Sprite[] foodSprites;
 
-
-    public float spawnRangeX = 9f;
-    public float spawnRangeY = 4f;
-
-    public float avoidRadius = 2f;
-
-    public Transform player; // 중심
+    public Transform player;
     public float shieldRadius = 2f;
 
+    Camera cam;
 
-    public void SpawnOneFood(float timeToHit)
+    void Awake()
     {
-        Vector2 spawnPos;
+        cam = Camera.main;
+    }
 
-        do
-        {
-            spawnPos = new Vector2(
-                Random.Range(-spawnRangeX, spawnRangeX),
-                Random.Range(-spawnRangeY, spawnRangeY)
-            );
-        }
-        while (spawnPos.magnitude < avoidRadius);
+    public void SpawnOneFood(float moveTime)
+    {
+        Vector3 spawnPos = GetOutsidePosition();
 
         GameObject food = Instantiate(foodPrefab, spawnPos, Quaternion.identity);
 
@@ -38,11 +28,47 @@ public class FoodSpawn_2_14 : MonoBehaviour
 
         if (sr != null && foodSprites.Length > 0)
         {
-            int rand = Random.Range(0, foodSprites.Length);
-            sr.sprite = foodSprites[rand];
+            sr.sprite = foodSprites[Random.Range(0, foodSprites.Length)];
         }
 
-        // 목표 위치 고정 (0,0,0) 으로 이동시키도록 Init 호출
-        food.GetComponent<FoodMove_2_14>()?.Init(timeToHit);
+        food.GetComponent<FoodMove_2_14>()
+            ?.Init(player, shieldRadius, moveTime);
+    }
+
+    Vector3 GetOutsidePosition()
+    {
+        Vector3 min = cam.ViewportToWorldPoint(new Vector3(0, 0, 10));
+        Vector3 max = cam.ViewportToWorldPoint(new Vector3(1, 1, 10));
+
+        float margin = 1.5f;
+
+        int side = Random.Range(0, 4);
+
+        switch (side)
+        {
+            case 0:
+                return new Vector3(
+                    min.x - margin,
+                    Random.Range(min.y, max.y),
+                    0);
+
+            case 1:
+                return new Vector3(
+                    max.x + margin,
+                    Random.Range(min.y, max.y),
+                    0);
+
+            case 2:
+                return new Vector3(
+                    Random.Range(min.x, max.x),
+                    max.y + margin,
+                    0);
+
+            default:
+                return new Vector3(
+                    Random.Range(min.x, max.x),
+                    min.y - margin,
+                    0);
+        }
     }
 }
